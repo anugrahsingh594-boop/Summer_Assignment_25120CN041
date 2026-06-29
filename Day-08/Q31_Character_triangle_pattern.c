f54e07c2f3d07e3bb299ff20bca12fb32d2dd9dc
anugrahsingh594-boop <anugrahsingh594@gmail.com>
2026-06-29 19:41:45 +0530

    Add files via upload

---
+++ Day-08/Q31_Character_triangle_pattern.c
@@ -0,0 +1,23 @@
+// print character triangle
+// A        
+// AB
+// ABC
+// ABCD
+// ABCDE
+
+#include <stdio.h>
+int main() 
+{
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) 
+    {
+        for (int j = 0; j < i; j++) 
+        {printf("%c", 'A' + j);}
+
+    printf("\n");
+    }
+    return 0;
+}
